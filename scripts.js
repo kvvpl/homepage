68fe52da31b576338ed6e14e3b99533fa4ec1ba0
Bartosz Markocki <bartosz.markocki@gmail.com>
2020-05-06 18:43:49 +0200

    Koniec tydzień4

--- scripts.js
+++ scripts.js
@@ -1,2 +1,7 @@
-alert("hello");
+// alert("hello");
 console.log("hello console");
+// const footer = document.querySelector('.dupa');
+const footer = document.querySelector(".main__section--js");
+console.log(footer);
+footer.innerHTML = "A to jest tekst z JS";
+console.log(footer);
