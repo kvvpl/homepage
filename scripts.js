8a9a9991c8447611d27e540afdd743f9ebf8b00d
Bartosz Markocki <bartosz.markocki@gmail.com>
2020-05-13 11:26:11 +0200

    End of week 5

--- scripts.js
+++ scripts.js
@@ -5,3 +5,9 @@ const footer = document.querySelector(".main__section--js");
 console.log(footer);
 footer.innerHTML = "A to jest tekst z JS";
 console.log(footer);
+
+const powitanie = (imie, wiek) => {
+  console.log(`Witaj ${imie} o wieku ${wiek}`);
+}
+
+powitanie("Bartek", 47);
