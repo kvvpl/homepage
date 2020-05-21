2850fec130bc31a0b465213fec3531ad1dbbd76e
Bartosz Markocki <bartosz.markocki@gmail.com>
2020-05-21 16:54:49 +0200

    Hamburger menu added

--- scripts.js
+++ scripts.js
@@ -11,3 +11,10 @@ const powitanie = (imie, wiek) => {
 }
 
 powitanie("Bartek", 47);
+
+const hamburger = document.querySelector('.button-js');
+console.log(hamburger);
+hamburger.addEventListener('click',(e)=>{
+  const navigation = document.querySelector('.navigation--js');
+  navigation.classList.toggle('navigation__open');
+})
